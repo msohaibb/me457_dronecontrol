ea0c3e0508a49f8cd69bd54ae4af8655322f5e37
Ben Aziel <70983779+destrospooder@users.noreply.github.com>
2022-10-09 22:01:35 -0400

    push

--- parameters/aerosonde_parameters.py
+++ parameters/aerosonde_parameters.py
@@ -9,15 +9,15 @@ from tools.rotations import Euler2Quaternion
 #   Initial conditions for MAV
 north0 = 0.  # initial north position
 east0 = 0.  # initial east position
-down0 = -100.0  # initial down position
-u0 = 25.  # initial velocity along body x-axis
+down0 = 0  # initial down position
+u0 = 25  # initial velocity along body x-axis
 v0 = 0.  # initial velocity along body y-axis
 w0 = 0.  # initial velocity along body z-axis
 phi0 = 0.  # initial roll angle
 theta0 = 0.  # initial pitch angle
 psi0 = 0.0  # initial yaw angle
 p0 = 0  # initial roll rate
-q0 = 0  # initial pitch rate
+q0 = 0.3  # initial pitch rate
 r0 = 0  # initial yaw rate
 Va0 = np.sqrt(u0**2+v0**2+w0**2)
 #   Quaternion State
